4241e3ba20b97fc592feda46a7cfd0e89295e6d8
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-03-12 11:40:24 +0800

    change certain colouring

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,24 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body class="font-sans">
+    <div class="min-vh-100 bg-dark">
+        @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-gray-800 shadow">
+                <div class="container py-4">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+        <!-- Page Content -->
+        <main class="container my-4">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
